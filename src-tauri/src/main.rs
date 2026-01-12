1ceec4c98596c19ac279e10f3aaa2090019f9210
zheng <1009552607@qq.com>
2026-01-12 21:56:23 +0800

    fix: 删除不存在的 tauri-plugin-path，移除 path 权限

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -222,7 +222,6 @@ fn main() {
 
     // Run Tauri Application
     tauri::Builder::default()
-        .plugin(tauri_plugin_path::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
